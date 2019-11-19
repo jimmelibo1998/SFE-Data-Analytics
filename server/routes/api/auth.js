81f8cfde11083d5ee79b2f588853b998ea8b5451
Jimmel Ibo <jimmel.ibo1998@yahoo.com>
2019-11-19 17:12:34 +0800

    Created login Route for admin

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require("express-validator");
 
 const MedRep = require("../../models/medRep");
 const Doctors = require("../../models/doctors");
+const Admin = require("../../models/admin");
 
 // @route POST api
 router.get("/", (req, res) => {
@@ -93,6 +94,61 @@ router.post(
           .json({ errors: [{ msg: "invalid credentials" }] });
 
       const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.send("Server Error");
+    }
+  }
+);
+
+// @route   POST api/auth/admin
+// @desc    Login admin
+// Access   Private
+router.post(
+  "/admin",
+  [
+    check("email", "email is required").isEmail(),
+    check("password", "password is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await Admin.findOne({ email });
+      if (!user)
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
       if (!isMatch)
         return res
           .status(400)
